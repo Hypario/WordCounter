f47df2a8b30e1de4a1718ae34e6c87dacab66ffa
Hypario <fabienduterte@mailfence.com>
2019-11-22 08:56:51 +0100

    removed mytext

--- counter.php
+++ counter.php
@@ -23,8 +23,6 @@
 
 <?php 
 
-//$mytext = "Finalement il revint sur les lèvres la flèche acérée et légère qui vole droit au but. Demande-leur s'ils l'emportent, et qui furent très partagés, comme le paysage qui les enchante, parce que la princesse vous expliquera elle-même. Bouffon, tu vas encore attraper froid, et on vexait son orgueil en sera consolé, mais son ombre même.";
-
 $occurences = [];
 
 function wordCount($text) {
